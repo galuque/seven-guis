c1fd12c21f0afda60454558ff20694e7504e1e13
Gabriel Luque <gldidonna@gmail.com>
2020-12-12 22:06:41 -0300

    organize timer

--- src/galuque/seven_guis/guis/timer.cljs
+++ src/galuque/seven_guis/guis/timer.cljs
@@ -6,32 +6,32 @@
                                          Slider]]
             [galuque.seven-guis.base.helpers :as h]))
 
-(defonce state (r/atom {:start (. js/Date now)
+(defonce state (r/atom {:start (.now js/Date)
                         :duration 50
                         :elapsed  0}))
 
 (defn timer []
-  (r/with-let [tick! (js/setInterval h/update-elapsed! 100 state)]
-    [:> Container {:max-width "xs"}
-     [:div "Elapsed Time: " (let [secs (h/display-elapsed @state)
-                                  prog (h/progress @state)]
-                              (if (<= prog 100)
-                                secs
-                                (h/display-final-time @state)))
-      " s"]
-     [:> LinearProgress {:variant "determinate"
-                         :value (let [prog (h/progress @state)]
-                                  (if (<= prog 100)
-                                    prog
-                                    100))}]
-     [:div "Duration"]
-     [:> Slider {:value (:duration @state)
-                 :on-change
-                 (fn [_ val]
-                   (swap! state assoc :duration val))}]
-     [:> Button {:on-click
-                 #(do
-                    (swap! state assoc :elapsed 0)
-                    (swap! state assoc :start (. js/Date now)))}
-      "Reset Timer"]]
-    (finally (js/clearInterval tick!))))
+  (let [secs (h/display-elapsed @state)
+        prog (h/progress @state)
+        final-elapsed (h/display-final-time @state)
+        elapsed-display (if (<= prog 100) secs final-elapsed)
+        prog-display (if (<= prog 100) prog 100)]
+    
+    (r/with-let [tick! (js/setInterval h/update-elapsed! 100 state)]
+      [:> Container {:max-width :xs}
+       [:div "Elapsed Time: " elapsed-display " s"]
+       [:> LinearProgress {:variant :determinate
+                           :value prog-display}]
+       [:div {:style {:margin-bottom :20px}}]
+       [:div "Duration"]
+       [:> Slider {:value (:duration @state)
+                   :on-change
+                   (fn [_ val]
+                     (swap! state assoc :duration val))}]
+       [:> Button {:variant :outlined
+                   :on-click
+                   #(do
+                      (swap! state assoc :elapsed 0)
+                      (swap! state assoc :start (.now js/Date)))}
+        "Reset Timer"]]
+      (finally (js/clearInterval tick!)))))
