238634a937d2dcaff8b4bb4abb627c864834179c
Gabriel Luque <gldidonna@gmail.com>
2020-12-15 18:18:10 -0300

    refactor booker component

--- src/galuque/seven_guis/guis/flight_booker.cljs
+++ src/galuque/seven_guis/guis/flight_booker.cljs
@@ -16,29 +16,45 @@
                                      (h/date-map->default-date))}))
 
 (defn flight-booker []
-  [:> Grid {:container true :align-items :center :justify :center :direction :column}
-   [:> TextField {:select true :default-value :one-way-flight}
-    [:> MenuItem {:value :one-way-flight
-                  :on-click #(swap! state update-in [:one-way?] not)}
-     "one-way flight"]
-    [:> MenuItem {:value :return-flight
-                  :on-click #(swap! state update-in [:one-way?] not)}
-     "return flight"]]
-   [:> TextField {:id :depart
-                  :type :date
-                  :default-value (:today @state)
-                  :on-change #(do
+  (let [toggle-flight-type   #(swap! state update-in [:one-way?] not)
+
+        handle-depart-change #(do
                                 (swap! state assoc :depart (new js/Date (.. % -target -value)))
-                                (swap! state assoc :valid-depart? (h/is-valid-depart? @state)))}]
-   [:> TextField {:id :return
-                  :type :date
-                  :default-value (:today @state)
-                  :disabled (:one-way? @state)
-                  :on-change #(do
+                                (swap! state assoc :valid-depart? (h/is-valid-depart? @state)))
+
+        handle-return-change #(do
                                 (swap! state assoc :return (new js/Date  (.. % -target -value)))
-                                (swap! state assoc :valid-return? (h/is-valid-return? @state)))}]
-   [:> Button {:variant :outlined
-               :disabled (or (not (:valid-return? @state))
-                             (not (:valid-depart? @state)))
-               :on-click #(js/alert (h/booked-message @state))}
-    "book"]])
+                                (swap! state assoc :valid-return? (h/is-valid-return? @state)))
+
+        handle-booked-click  #(js/alert (h/booked-message @state))
+
+        button-disabled?      (not (and (:valid-depart? @state) 
+                                        (:valid-return? @state)))]
+    
+    [:> Grid {:container true :align-items :center :justify :center :direction :column}
+     [:> TextField {:select true :default-value :one-way-flight}
+      [:> MenuItem {:value :one-way-flight
+                    :on-click toggle-flight-type}
+       "one-way flight"]
+      [:> MenuItem {:value :return-flight
+                    :on-click toggle-flight-type}
+       "return flight"]]
+     [:> TextField {:id :depart
+                    :type :date
+                    :default-value (:today @state)
+                    :on-change handle-depart-change}]
+     [:> TextField {:id :return
+                    :type :date
+                    :default-value (:today @state)
+                    :disabled (:one-way? @state)
+                    :on-change handle-return-change}]
+     [:> Button {:variant :outlined
+                 :disabled button-disabled?
+                 :on-click handle-booked-click}
+      "book"]]))
+
+(comment
+  (<= (new js/Date) (:depart @state))
+  (h/is-valid-depart? @state)
+  (<= (new js/Date (:today @state)) (:depart @state))
+  (> 19 18 17))
