73a90d1ed06d1dba4fccca00f3c7ac98b3d97d44
Gabriel Luque <gldidonna@gmail.com>
2020-12-15 18:18:25 -0300

    refactor timer component

--- src/galuque/seven_guis/guis/timer.cljs
+++ src/galuque/seven_guis/guis/timer.cljs
@@ -11,13 +11,25 @@
                         :elapsed  0}))
 
 (defn timer []
-  (let [secs (h/display-elapsed @state)
-        prog (h/progress @state)
-        final-elapsed (h/display-final-time @state)
-        elapsed-display (if (<= prog 100) secs final-elapsed)
-        prog-display (if (<= prog 100) prog 100)]
+  (let [secs                   (h/display-elapsed @state)
+        
+        prog                   (h/progress @state)
+        
+        final-elapsed          (h/display-final-time @state)
+        
+        elapsed-display        (if (<= prog 100) secs final-elapsed)
+        
+        prog-display           (if (<= prog 100) prog 100)
+        
+        handle-durarion-change (fn [_ val]
+                                 (swap! state assoc :duration val))
+        
+        handle-reset         #(do
+                                (swap! state assoc :elapsed 0)
+                                (swap! state assoc :start (.now js/Date))) ]
     
     (r/with-let [tick! (js/setInterval h/update-elapsed! 100 state)]
+
       [:> Container {:max-width :xs}
        [:div "Elapsed Time: " elapsed-display " s"]
        [:> LinearProgress {:variant :determinate
@@ -25,13 +37,9 @@
        [:div {:style {:margin-bottom :20px}}]
        [:div "Duration"]
        [:> Slider {:value (:duration @state)
-                   :on-change
-                   (fn [_ val]
-                     (swap! state assoc :duration val))}]
+                   :on-change handle-durarion-change}]
        [:> Button {:variant :outlined
-                   :on-click
-                   #(do
-                      (swap! state assoc :elapsed 0)
-                      (swap! state assoc :start (.now js/Date)))}
+                   :on-click handle-reset}
         "Reset Timer"]]
+                
       (finally (js/clearInterval tick!)))))
