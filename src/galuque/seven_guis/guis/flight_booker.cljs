70da4c402c9a0829b93b541bbb30f84a7d1022f4
Gabriel Luque <gldidonna@gmail.com>
2021-03-27 22:53:28 -0300

    Eliminate bugs by using google closure date object

--- src/galuque/seven_guis/guis/flight_booker.cljs
+++ src/galuque/seven_guis/guis/flight_booker.cljs
@@ -5,7 +5,8 @@
                                          TextField]]
             [cljs.core.async :as async]
             [galuque.seven-guis.base.helpers :as h :refer [events->chan by-id]]
-            [reagent.core :as r])
+            [reagent.core :as r]
+            [goog.date :as date])
   (:import [goog.events EventType]))
 
 (def styles {:grid {:container true :align-items :center :justify :center :direction :column}
@@ -14,16 +15,10 @@
              
              :option {:class "MuiSelect-root MuiSelect-select MuiSelect-selectMenu MuiInputBase-input MuiInput-input"}})
 
-(defonce state (r/atom {:depart (js/Date.)
-                        :return (js/Date.)
-                        :one-way? true
-                        :valid-return? false
-                        :valid-depart? false}))
-
-(defn coordinate []
+(defn coordinate! [state]
   (let [toggle-flight-type  (events->chan (by-id "input-select") EventType.CHANGE)
-        depart-change       (events->chan (by-id "depart")       EventType.CHANGE)
-        return-change       (events->chan (by-id "return")       EventType.CHANGE)
+        depart-change       (events->chan (by-id "depart-date")  EventType.CHANGE)
+        return-change       (events->chan (by-id "return-date")  EventType.CHANGE)
         book-click          (events->chan (by-id "book-button")  EventType.CLICK)]
     (async/go-loop []
       (async/alt!
@@ -32,14 +27,14 @@
 
         depart-change
         ([e]
-         (swap! state assoc :depart (js/Date. (.. e -target -value)))
+         (println (.. e -target -value))
+         (swap! state assoc :depart (date/Date. (.. e -target -value)))
          (swap! state assoc :valid-depart? (h/valid-depart? @state))
-         (swap! state assoc :valid-return? (h/valid-return? @state))
          (recur))
 
         return-change
         ([e]
-         (swap! state assoc :return (js/Date.  (.. e -target -value)))
+         (swap! state assoc :return (date/Date.  (.. e -target -value)))
          (swap! state assoc :valid-return? (h/valid-return? @state))
          (recur))
 
@@ -47,32 +42,35 @@
         ([_] (js/alert (h/booked-message @state)) (recur))))))
 
 (defn flight-booker []
-  (r/create-class
-   {:display-name "Flight Booker"
+  (let [state (r/atom {:depart (date/Date.)
+                       :return (date/Date.)
+                       :one-way? true
+                       :valid-depart? true
+                       :valid-return? true})]
+    (r/create-class
+     {:display-name "Flight Booker"
 
-    :component-did-mount
-    (fn [] (coordinate))
+      :component-did-mount
+      (fn [] (coordinate! state))
 
-    :reagent-render
-    (fn []
-      (let [button-disabled? (not (or (and (:one-way? @state) (:valid-depart? @state))
-                                      (and (:valid-depart? @state) (:valid-return? @state))))]
-        [:> Grid (:grid styles)
-         [:select  (:flight-type styles)
-          [:option (merge (:option styles) {:value :one-way-flight}) "one-way flight"]
-          [:option (merge (:option styles) {:value :return-flight}) "return flight"]]
-         [:> TextField {:id :depart
-                        :type :date
-                        :default-value (-> (js/Date.) (h/date->map) (h/date-map->default-date))}]
-         [:> TextField {:id :return
-                        :type :date
-                        :disabled (:one-way? @state)}]
-         [:> Button {:id :book-button
-                     :variant :outlined
-                     :disabled button-disabled?}
-          "book"]]))}))
-
-(comment
-  (h/is-valid-depart? @state)
-  (<= (js/Date.) (:depart @state)) ;; BUG: js/Date. returns UTC time, after 10 pm ART it breaks
-  ) 
+      :reagent-render
+      (fn []
+        (let [button-disabled? (not (or (and (:one-way? @state) (:valid-depart? @state))
+                                        (and (:valid-depart? @state) (:valid-return? @state))))]
+          [:> Grid (:grid styles)
+           [:select  (:flight-type styles)
+            [:option (merge (:option styles) {:value :one-way-flight})
+             "one-way flight"]
+            [:option (merge (:option styles) {:value :return-flight})
+             "return flight"]]
+           [:> TextField {:id :depart-date
+                          :type :date
+                          :default-value (-> (date/Date.) (h/->date-str "-"))}]
+           [:> TextField {:id :return-date
+                          :type :date
+                          :disabled (:one-way? @state)
+                          :default-value (-> (date/Date.) (h/->date-str "-"))}]
+           [:> Button {:id :book-button
+                       :variant :outlined
+                       :disabled button-disabled?}
+            "book"]]))})))
