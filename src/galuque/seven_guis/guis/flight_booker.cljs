f607089d2e13d5a62c1cb5736ef36128b6bc89ca
Gabriel Luque <gldidonna@gmail.com>
2021-03-29 12:08:06 -0300

    Clean up Flight booker

--- src/galuque/seven_guis/guis/flight_booker.cljs
+++ src/galuque/seven_guis/guis/flight_booker.cljs
@@ -1,25 +1,27 @@
 (ns galuque.seven-guis.guis.flight-booker
-  (:require ["@material-ui/core" :refer [Button 
-                                         Grid 
-                                         MenuItem 
+  (:require ["@material-ui/core" :refer [Button
+                                         Grid
+                                         MenuItem
                                          TextField]]
             [cljs.core.async :as async]
-            [galuque.seven-guis.base.helpers :as h :refer [events->chan by-id]]
-            [reagent.core :as r]
-            [goog.date :as date])
+            [galuque.seven-guis.base.helpers :as h :refer [events->chan by-id log]]
+            [goog.date :as date]
+            [reagent.core :as r])
   (:import [goog.events EventType]))
 
 (def styles {:grid {:container true :align-items :center :justify :center :direction :column}
 
              :flight-type {:id :input-select :name :input-select :class "MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-formControl MuiInput-formControl"}
-             
+
              :option {:class "MuiSelect-root MuiSelect-select MuiSelect-selectMenu MuiInputBase-input MuiInput-input"}})
 
 (defn coordinate! [state]
   (let [toggle-flight-type  (events->chan (by-id "input-select") EventType.CHANGE)
         depart-change       (events->chan (by-id "depart-date")  EventType.CHANGE)
         return-change       (events->chan (by-id "return-date")  EventType.CHANGE)
-        book-click          (events->chan (by-id "book-button")  EventType.CLICK)]
+        book-click          (events->chan (by-id "book-button")  EventType.CLICK)
+        str->date           (fn [s]
+                              (.fromIsoString ^js date/Date s))]
     (async/go-loop []
       (async/alt!
         toggle-flight-type
@@ -27,15 +29,17 @@
 
         depart-change
         ([e]
-         (swap! state assoc :depart (date/Date. (.. e -target -value)))
-         (swap! state assoc :valid-depart? (h/valid-depart? @state))
-         (recur))
+         (let [value (.. e -target -value)]
+           (swap! state assoc :depart (str->date value))
+           (swap! state assoc :valid-depart? (h/valid-depart? @state))
+           (recur)))
 
         return-change
         ([e]
-         (swap! state assoc :return (date/Date.  (.. e -target -value)))
-         (swap! state assoc :valid-return? (h/valid-return? @state))
-         (recur))
+         (let [value (.. e -target -value)]
+           (swap! state assoc :return (str->date value))
+           (swap! state assoc :valid-return? (h/valid-return? @state))
+           (recur)))
 
         book-click
         ([_] (js/alert (h/booked-message @state)) (recur))))))
@@ -54,8 +58,8 @@
 
       :reagent-render
       (fn []
-        (let [default-date     (-> (date/Date.) (h/->date-str "-"))
-              button-disabled? (not (or (and (:one-way? @state) (:valid-depart? @state))
+        (let [default-value    (-> (date/Date.) (h/->date-str "-"))
+              button-disabled? (not (or (and (:valid-depart? @state) (:one-way? @state))
                                         (and (:valid-depart? @state) (:valid-return? @state))))]
           [:> Grid (:grid styles)
            [:select  (:flight-type styles)
@@ -65,11 +69,11 @@
              "return flight"]]
            [:> TextField {:id :depart-date
                           :type :date
-                          :default-value default-date}]
+                          :default-value default-value}]
            [:> TextField {:id :return-date
                           :type :date
                           :disabled (:one-way? @state)
-                          :default-value default-date}]
+                          :default-value default-value}]
            [:> Button {:id :book-button
                        :variant :outlined
                        :disabled button-disabled?}
